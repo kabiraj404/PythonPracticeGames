15e786449faca36d68b127e6a94dea0cfae31465
kabiraj404 <59503311+>
2020-11-06 21:25:21 +0545

    added_KarobarDaily

--- 4.NepaliNewsPaperPDF.py
+++ 4.NepaliNewsPaperPDF.py
@@ -18,8 +18,17 @@ open("D:/todayPaper/Kantipur_daily."+ str(d3)+'.pdf', 'wb').write(kan.content)
 
 urlktmpost = "https://epaper.ekantipur.com/kathmandupost/download/" + str(d3)	
 kan = requests.get(urlktmpost, allow_redirects = True)
-open("D:/todayPaper/KathmanduPost_daily."+ str(d3)+'.pdf', 'wb').write(kan.content)	
+open("D:/todayPaper/KathmanduPost_daily_"+ str(d3)+'.pdf', 'wb').write(kan.content)	
 	
+##for karobar dailey
+d4 = today.strftime("%d-%b-%Y")
+year = today.strftime("%Y")
+month = today.strftime("%m")
+
+urlkarobar = "https://www.karobardaily.com/wp-content/uploads/" + str(year) + "/" + str(month) + "/Karobar-" + str(d4) + ".pdf"
+karobar = requests.get(urlkarobar, allow_redirects = True)
+open("D:/todayPaper/Karobar_daily_"+ str(d3)+'.pdf', 'wb').write(karobar.content)	
+
 
 ##for Gorkhapatra 
 driver = webdriver.Firefox(executable_path="D:/OneDrive - ICIMOD/python/seleniumGekoDriver/geckodriver.exe")
@@ -53,14 +62,15 @@ driver.set_window_position(1, 1)
 driver.get("https://www.nepalitimes.com/")
 urlgor = driver.find_elements_by_xpath("/html/body/div[1]/div[4]/div/main/section[1]/div/div[1]/div[2]/div[1]/div[6]/a")[0]
 urlgor.click()
+
 driver.switch_to.window(driver.window_handles[1])
 time.sleep(5)
 url3 = driver.current_url
 
 gp = requests.get(url3, allow_redirects = True)
-open("D:/todayPaper/NepaliTimes_weekly"+str(d3)+ ".pdf", mode="wb").write(gp.content)
+open("D:/todayPaper/NepaliTimes_weekly_"+str(d3)+ ".pdf", mode="wb").write(gp.content)
 
 driver.close()
 driver.quit()
 
-print("Yahoo!! you have successfully downloaded Kantipur, Kathmandu Post, Gorkhapatra and Nepali Times pdf") 
+print("Yahoo!! you have successfully downloaded Kantipur, Kathmandu Post, Gorkhapatra, Karobar and Nepali Times pdf") 
